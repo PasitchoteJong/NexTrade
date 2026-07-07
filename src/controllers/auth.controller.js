dc61ce159b8d7c630aa8eeeb811a68b6d7a6fe0a
PasitJong <jong.pasitchote@gmail.com>
2026-07-07 15:41:56 +0700

    register v1 done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,36 @@
-export function register(req, res) {
-    res.send('Register Controller')
+import createHttpError from "http-errors"
+import { prisma } from "../lib/prisma.js";
+import bcrypt from "bcryptjs"
+
+
+export async function register(req, res, next) {
+    const { firstName, lastName, email, mobile, password, confirmPassword } = req.body
+
+    if (!firstName.trim() || !lastName.trim() || !email.trim() || !mobile.trim() || !password.trim() || !confirmPassword.trim()) {
+        return next(createHttpError[400]('fill all inputs'))
+    }
+    if (confirmPassword !== password) {
+        return next(createHttpError[400]('check confirm-password'))
+    }
+    const haveUser = await prisma.user.findUnique({
+        where: { email }
+    })
+    if (haveUser) {
+        return next(createHttpError[409]('email is already registered.'))
+    }
+    const newUser = {
+        email,
+        password: await bcrypt.hash(password, 10),
+        firstName,
+        lastName,
+        mobile
+    }
+    const result = await prisma.user.create({ data: newUser })
+    res.json({
+        message: 'Register Successful',
+        result
+    })
+
 }
 
 export function login(req, res) {
