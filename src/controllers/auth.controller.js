16e4575095e15e513e5def020043a7aa038df1bf
PasitJong <jong.pasitchote@gmail.com>
2026-07-10 16:54:44 +0700

    error-middleware

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,34 +1,35 @@
 import createHttpError from "http-errors"
 import { prisma } from "../lib/prisma.js";
-import bcrypt from "bcryptjs"
+import { registerSchema } from "../validations/schema.js";
 
+export async function register(req, res, next){
+    //const { firstName, lastName, email, mobile, password, confirmPassword } = req.body
+    // console.log(req.body)
+    const data = await registerSchema.parseAsync(req.body)
 
-export async function register(req, res, next) {
-    const { firstName, lastName, email, mobile, password, confirmPassword } = req.body
-
-    if (!firstName.trim() || !lastName.trim() || !email.trim() || !mobile.trim() || !password.trim() || !confirmPassword.trim()) {
-        return next(createHttpError[400]('fill all inputs'))
-    }
-    if (confirmPassword !== password) {
-        return next(createHttpError[400]('check confirm-password'))
-    }
+    // if (!firstName.trim() || !lastName.trim() || !email.trim() || !mobile.trim() || !password.trim() || !confirmPassword.trim()) {
+    //     return next(createHttpError[400]('fill all inputs'))
+    // }
+    // if (confirmPassword !== password) {
+    //     return next(createHttpError[400]('check confirm-password'))
+    // }
     const haveUser = await prisma.user.findUnique({
-        where: { email }
+        where: { email: data.email }
     })
     if (haveUser) {
         return next(createHttpError[409]('email is already registered.'))
     }
-    const newUser = {
-        email,
-        password: await bcrypt.hash(password, 10),
-        firstName,
-        lastName,
-        mobile
-    }
-    const result = await prisma.user.create({ data: newUser })
+    // const newUser = {
+    //     email: data.email,
+    //     password: await bcrypt.hash(data.password, 10),
+    //     firstName: data.firstName,
+    //     lastName: data.lastName,
+    //     mobile: data.mobile
+    // }
+    const result = await prisma.user.create({ data: data })
     res.json({
         message: 'Register Successful',
-        result
+        result : result
     })
 
 }
---
+++ src/validations/schema.js
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import bcrypt from "bcryptjs";
+
+const mobileRegex = /^[0-9]{10,15}$/
+
+export const registerSchema = z.object({
+    email: z.string()
+        .min(2, "email require")
+        .email("Must be valid email"),
+    firstName: z.string()
+        .min(2, "firstname is required"),
+    lastName: z.string()
+        .min(2, "lastname is required"),
+    mobile: z.string()
+        .min(10, "mobile number required")
+        .refine(value => mobileRegex.test(value), {
+            message: "Must be valid mobile phone"
+        }),
+
+    password: z.string()
+        .min(8, "password at least 8 characters")
+        .regex(/[a-z]/, "must contain lowercase 1 characters")
+        .regex(/[A-Z]/, "must contain uppercase 1 characters")
+        .regex(/[0-9]/, "must contain number 1 characters"),
+    confirmPassword: z.string()
+        .min(1, "confrim password is required"),
+}).refine(data => data.password === data.confirmPassword, {
+    message: " confirm password not match password",
+    path: ['confirmPassword']
+}).transform(async data=>{
+    const output = {
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        mobile: data.mobile,
+        password: await bcrypt.hash(data.password,8)
+    }
+    return output
+})
