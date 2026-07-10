7fa57fc74bc3ebcf973e595315260ffcb36aa632
PasitJong <jong.pasitchote@gmail.com>
2026-07-10 18:14:59 +0700

    JWT Token Error

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,17 +1,30 @@
 import { ZodError } from "zod"
 
 export default function (err,req,res,next){
-    if(err instanceof ZodError){
-        console.log(err.flatten())
-        return res.status(400).json({
-            message: 'Validation Error',
-            errors: err.flatten().fieldErrors
-        })
+    if(err.name === 'TokenExpriedError'){
+        return res.status(401).json({
+            error: 'Token Expried',
+            message: 'Your session has expired. Please log in again.'
+        });
     }
-    console.error(err)
-    res.status(err.status || 500)
-    res.json({
-        status : err.status || 500,
-        message : err.message || 'Server Error'
-    })
+    if(err.name === 'JsonWebTokenError'){
+        return res.status(401).json({
+            error: 'Invalid Token',
+            message: 'Theprovide is invalid or malformed.'
+        });
+    }
+
+    // if(err instanceof ZodError){
+    //     console.log(err.flatten())
+    //     return res.status(400).json({
+    //         message: 'Validation Error',
+    //         errors: err.flatten().fieldErrors
+    //     })
+    // }
+    // console.error(err)
+    // res.status(err.status || 500)
+    // res.json({
+    //     status : err.status || 500,
+    //     message : err.message || 'Server Error'
+    // })
 }
